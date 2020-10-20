9190beb06eef0df250ea55e9d156293495206d16
AceCello28 <70405943+AceCello28@users.noreply.github.com>
2020-10-20 12:49:40 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,76 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("I Love History!!!")
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        # . . . #
+        # # . # #
+        # . . # #
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . # # #
+        # . . . #
+        # # . # #
+        # . . # #
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . # # #
+        # # . . #
+        # # . # #
+        # . . # #
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # . .
+        # . # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . # #
+        # # # # #
+        `)
+})
